987ea432eead6092b600889fb5ba37c4c654c905
Hugo Branquinho <ohugobranquinho@icloud.com>
2019-09-02 17:06:10 -0300

    gerenate controller communities

---
+++ app/controllers/communities_controller.rb
@@ -0,0 +1,5 @@
+class CommunitiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # routes to community
+  resources :communities, only: :show
 end
