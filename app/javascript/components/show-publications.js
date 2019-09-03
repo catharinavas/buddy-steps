bc44054157087bfae31b3cc18414bf418bd24e75
Hugo Branquinho <ohugobranquinho@icloud.com>
2019-09-03 15:14:16 -0300

    uma hora vai ...

--- app/javascript/components/show-publications.js
+++ app/javascript/components/show-publications.js
@@ -2,11 +2,29 @@ const showPublications = () => {
   const menuNews = document.getElementById('menu-news');
   const menuNewsList = document.getElementById('menu-news-list');
 
+  const menuCelebrations = document.getElementById('menu-celebrations');
+  const menuCelebrationsList = document.getElementById('menu-celebrations-list');
+
+  const menuUsers = document.getElementById('menu-users');
+  const menuUsersList = document.getElementById('menu-user-list');
+
   menuNews.addEventListener('click', () => {
-    menuNewsList.classList.toggle('hidden');
+    menuNewsList.classList.remove('hidden');
+    menuCelebrationsList.classList.add('hidden');
+    menuUsersList.classList.add('hidden');
+  })
+
+  menuCelebrations.addEventListener('click', () => {
+    menuNewsList.classList.add('hidden');
+    menuCelebrationsList.classList.remove('hidden');
+    menuUsersList.classList.add('hidden');
+  })
+
+  menuUsers.addEventListener('click', () => {
+    menuNewsList.classList.add('hidden');
+    menuCelebrationsList.classList.add('hidden');
+    menuUsersList.classList.remove('hidden');
   })
 }
 
 export { showPublications };
-  // const menuCelebrations = document.getElementById('menu-celebrations');
-  // const menuUser = document.getElementById('menu-user');
