f5e4f9a9d67f32381e26837fdd4ce0275cf177de
Daniela Ichikawa <danichikawa@gmail.com>
2019-09-04 00:19:44 -0300

    responsive chat

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,15 @@
 import '../css/application.css';
 import "../plugins/flatpickr";
 import { menuToggle, dropdownToggle, closeMenu } from '../components/navbar.js';
-import { showPublications } from '../components/show-publications.js';
-import {modal} from '../components/modal';
+// import { showPublications } from '../components/show-publications.js';
+import { addMilestone } from '../components/add_milestone.js';
+import { chat } from '../components/chat.js';
+import { modal } from '../components/modal';
 
 menuToggle();
 dropdownToggle();
 closeMenu();
-showPublications();
-modal()
+// showPublications();
+addMilestone();
+chat();
+modal();
