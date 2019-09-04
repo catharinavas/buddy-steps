e8d7345b9067e193540c83eff579978d4f282749
Hugo Branquinho <ohugobranquinho@icloud.com>
2019-09-04 09:25:11 -0300

    init solution

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import '../css/application.css';
 import "../plugins/flatpickr";
 import { menuToggle, dropdownToggle, closeMenu } from '../components/navbar.js';
-// import { showPublications } from '../components/show-publications.js';
+import { showPublications } from '../components/show-publications.js';
 import { addMilestone } from '../components/add_milestone.js';
 import { chat } from '../components/chat.js';
 import { modal } from '../components/modal';
@@ -9,7 +9,7 @@ import { modal } from '../components/modal';
 menuToggle();
 dropdownToggle();
 closeMenu();
-// showPublications();
+showPublications();
 addMilestone();
 chat();
 modal();
