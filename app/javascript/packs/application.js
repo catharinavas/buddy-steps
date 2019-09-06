a9b52ae98419f4530d30af0b70269a7dce725678
Hugo Branquinho <ohugobranquinho@icloud.com>
2019-09-06 12:39:05 -0300

    move tailswind to asset pipeline

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,3 @@
-// import '../css/application.css';
 import "../plugins/flatpickr";
 import { menuToggle, dropdownToggle, closeMenu } from '../components/navbar';
 import { showPublications } from '../components/show-publications';
