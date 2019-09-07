6bb7f415c616d1d3961fb1d8e1757ef0e0352ba1
Hugo Branquinho <ohugobranquinho@icloud.com>
2019-09-07 17:58:25 -0300

    Install search pg on community show

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -13,9 +13,22 @@ class CommunitiesController < ApplicationController
 
   def show
     @publication = Publication.new
-    @users = @community.users
     @type = PublicationType.where(name: ['News', 'Question'])
-    @community_publications = Publication.where(community: params[:id], publication_type: [@type[0].id, @type[1].id])
+
+    if params[:query].present?
+      @community_publications = Publication.where(community: params[:id], publication_type: [@type[0].id, @type[1].id])
+      sql_query = " \
+        publications.title ILIKE :query \
+        OR publications.content ILIKE :query \
+        OR users.first_name ILIKE :query \
+        OR users.last_name ILIKE :query \
+      "
+      @community_publications = @community_publications.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @community_publications = Publication.where(community: params[:id], publication_type: [@type[0].id, @type[1].id])
+    end
+
+    @users = @community.users
   end
 
   private
